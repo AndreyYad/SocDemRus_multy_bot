7e1e9be07e4cc36491a7b09ca7bd9a9931e47b7d
RubyRed <andrey200520059@gmail.com>
2023-09-06 01:22:36 +0300

    Установка состояния на получения сообщения и сброс состояний

--- handlers/client.py
+++ handlers/client.py
@@ -1,13 +1,39 @@
-from aiogram import types, Dispatcher
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from modules.bot_cmds import *
 from modules.messages import MESSAGES
 from modules.bot_dispatcher import dp
 
-# @dp.message_handler(commands=['start'])
+class FSMClient(StatesGroup):
+    anonim_msg_text = State()
+
+# commands=['start']
 async def start_func(msg: types.Message):
     if msg.chat.type == 'private':
-        await send_msg(msg.from_user.id, MESSAGES['start'])
+        await msg.reply(MESSAGES['start'])
+
+# commands=['cancel']
+async def cancel_func(msg: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        if data.state != None:
+            await state.finish()
+            await msg.reply(MESSAGES['reset'])
+
+# commands=['msg_ok']
+async def cmd_send_anonim_msg_func(msg: types.Message):
+    if msg.chat.type == 'private':
+        await FSMClient.anonim_msg_text.set()
+        await msg.reply(MESSAGES['anonim_msg_info'])
+
+# state=FSMClient.anonim_msg_text
+async def get_anonim_msg_func(msg: types.Message, state: FSMContext):
+    await msg.reply(msg.text)
+    await state.finish()
 
 def register_handlers_client():
-    dp.register_message_handler(start_func, commands=['start'])
+    dp.register_message_handler(start_func, commands=['start'], state=None)
+    dp.register_message_handler(cancel_func, commands=['cancel'], state='*')
+    dp.register_message_handler(cmd_send_anonim_msg_func, commands=['msg_ok'], state=None)
+    dp.register_message_handler(get_anonim_msg_func, state=FSMClient.anonim_msg_text)
